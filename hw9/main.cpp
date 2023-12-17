7bcea9a020afed5041c87ee36165007dab9e0a70
te0r1ty <max.polyakov@inbox.ru>
2023-12-17 22:14:50 +0300

    Threads ans number generation

--- hw9/main.cpp
+++ hw9/main.cpp
@@ -1,6 +1,48 @@
 #include <iostream>
+#include <pthread.h>
+#include <windows.h>
+#include <random>
+
+const int threadNumber = 20;
+
+void *Number(void *param) {    //вычисление суммы квадратов в потоке
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> timeRand(1, 7);
+    std::uniform_int_distribution<> numRand(1, 20);
+
+    int n = timeRand(gen);
+    Sleep(n * 1000);
+
+    int* num = (int*)param;
+    std::cout << "Generated #" << *num << " number\n";
+
+    *num = numRand(gen);
+
+    return nullptr;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    srand((unsigned)time(nullptr));
+    int nums[threadNumber];
+    for (int i = 0; i < threadNumber; ++i) {
+        nums[i] = i + 1;
+    }
+    pthread_t threads[threadNumber];
+
+
+    for (int i = 0; i < threadNumber; ++i) {
+        pthread_create(&threads[i], nullptr, Number, (void *) &nums[i]);
+    }
+
+
+
+
+    for (auto thread : threads) {    // ожидание завершения работы дочерних потоков
+        pthread_join(thread, nullptr);
+    }
+    for (int num : nums) {
+        std::cout << num << std::endl;
+    }
     return 0;
 }
