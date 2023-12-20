d5635f3b3881506a85dbd5d157f80b09da3c64cc
te0r1ty <max.polyakov@inbox.ru>
2023-12-20 07:55:58 +0300

    Stable working version

---
+++ ihw4/pin.h
@@ -0,0 +1,27 @@
+enum class Sharp : int {Dull = 0, Sharp = 1, VerySharp = 2};
+
+class Pin {
+private:
+    bool usable;
+    bool straight;
+    Sharp sharpness;
+public:
+    explicit Pin(bool straight) {
+        this->usable = false;
+        this->straight = straight;
+        this->sharpness = Sharp::Dull;
+    }
+    void setUsable(bool val) {
+        this->usable = val;
+    }
+    [[nodiscard]] bool isStraight() const {
+        return this->straight;
+    }
+    [[nodiscard]] int isSharpened() const {
+        return (int)this->sharpness;
+    }
+    void setSharpened(int val) {
+        this->sharpness = (Sharp)val;
+    }
+    virtual ~Pin() = default;
+};
